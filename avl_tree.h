1243b3f0881ffb42899738d1841d2f4754d2f22d
Patrik119HUN <tukacs.patrik77@gmail.com>
2024-02-20 10:15:09 +0100

    FIX height calc on insert

--- avl_tree.h
+++ avl_tree.h
@@ -9,11 +9,24 @@ struct Node {
     T value;
 
     uint8_t height{0};
-    uint8_t children_count{0};
 
     Node* parent{nullptr};
     Node* left{nullptr};
     Node* right{nullptr};
+
+    static uint8_t get_height(Node<T>* p_node) {
+        if (p_node == nullptr) return 0;
+        return p_node->height;
+    }
+
+    static uint8_t get_balance_factor(Node<T>* p_node) {
+        if (p_node == nullptr) return 0;
+        return get_height(p_node->left) - get_height(p_node->right);
+    }
+
+    uint8_t get_height() { return this->height; }
+
+    uint8_t get_balance_factor() { return Node<T>::get_balance_factor(this); }
 
     static Node<T>* insert(Node<T>* p_node, Node<T>* p_parent, const T &value) {
         if (p_node == nullptr) {
@@ -23,24 +36,34 @@ struct Node {
         if (p_node->value > value) {
             p_node->right = insert(p_node->right, p_node, value);
         } else { p_node->left = insert(p_node->left, p_node, value); }
+        //find_hegiht(p_node);
+        p_node->height = 1 + std::max(get_height(p_node->right), get_height(p_node->left));
         return p_node;
     }
 
-    static Node<T>* breath_first_search(Node<T>* p_node) {
+    static void breadth_first_search(Node<T>* p_node, std::function<void(Node<T>*)> function) {
         std::queue<Node<T>*> queue;
         queue.push(p_node);
         while (!queue.empty()) {
             Node<T>* temp = queue.front();
             queue.pop();
-
+            function(temp);
             if (temp->left != nullptr) queue.push(temp->left);
             if (temp->right != nullptr) queue.push(temp->right);
         }
     }
+
+    static void left_rotate(Node<T>* p_node) {
+
+    }
+
+    static void right_rotate(Node<T>* p_node) {
+
+    }
 };
 
 template<typename T>
-uint8_t find_hegiht(Node<T>* t_node, T value) {
+uint8_t find_hegiht(Node<T>* t_node) {
     if (t_node == nullptr) return -1;
     uint8_t height = -1;
     std::queue<Node<T>*> queue;
@@ -129,6 +152,10 @@ public:
     }
 
     iterator begin() { return iterator(m_root); }
+
+    Node<T>* get_root() {
+        return m_root;
+    }
 
 private:
     Node<T>* m_root = nullptr;
--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "avl_tree.h"
+
 int main() {
     AVLTree<int> fa;
     fa.insert(2);
     fa.insert(5);
     fa.insert(1);
     fa.insert(4);
+    Node<int>::breadth_first_search(fa.get_root(), [](Node<int>* temp) { std::cout << temp->value << std::endl; });
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
